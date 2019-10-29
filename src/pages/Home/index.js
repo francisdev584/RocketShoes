fcc1a1537f6f341b709ce2db400a5766866c6458
Francinildo <francis584@gmail.com>
2019-10-29 13:29:31 -0300

    Home style - done.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,118 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src={
+            'https://static.netshoes.com.br/produtos/05/HZM-1276-405/HZM-1276-405_detalhe1.jpg?ims=280x280'
+          }
+          alt="tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src={
+            'https://static.netshoes.com.br/produtos/05/HZM-1276-405/HZM-1276-405_detalhe1.jpg?ims=280x280'
+          }
+          alt="tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src={
+            'https://static.netshoes.com.br/produtos/05/HZM-1276-405/HZM-1276-405_detalhe1.jpg?ims=280x280'
+          }
+          alt="tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src={
+            'https://static.netshoes.com.br/produtos/05/HZM-1276-405/HZM-1276-405_detalhe1.jpg?ims=280x280'
+          }
+          alt="tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src={
+            'https://static.netshoes.com.br/produtos/05/HZM-1276-405/HZM-1276-405_detalhe1.jpg?ims=280x280'
+          }
+          alt="tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src={
+            'https://static.netshoes.com.br/produtos/05/HZM-1276-405/HZM-1276-405_detalhe1.jpg?ims=280x280'
+          }
+          alt="tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
