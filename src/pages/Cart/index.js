3317089cb2262ae7b2aa6096c8addc62340e20b0
Francinildo <francis584@gmail.com>
2019-10-31 19:17:20 -0300

    add subtotal and total on Cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -10,8 +10,9 @@ import {
 
 import * as CartActions from '../../store/modules/cart/actions';
 import { Container, ProductTable, Total } from './styles';
+import { formatPrice } from '../../util/format';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1);
   }
@@ -52,7 +53,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </button>
               </td>
               <td>
-                <strong>R$258,80</strong>
+                <strong>{product.subtotal}</strong>
               </td>
               <td>
                 <button
@@ -70,7 +71,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
         <button type="button">Finalizar Pedido</button>
         <Total>
           <span>TOTAL</span>
-          <strong>R$129,90</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
@@ -78,7 +79,15 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 }
 
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
 });
 
 const mapDispatchToProps = dispatch =>
